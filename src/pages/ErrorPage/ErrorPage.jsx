34224e94f711aaaaeacc72fad2cf62ed0bc4fa47
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-05-04 23:55:11 +0600

    error page fixed

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -5,6 +5,8 @@ function ErrorPage() {
     <div className="error-page">
       <h1 className="text-center text-danger mt-5">404 Error: Page Not Found</h1>
       <p className="text-center">Sorry, the page you are looking for does not exist.</p>
+      <img className="mx-auto d-block" style={{ height: "300px", width: "400px", color: "#f90606" }} src="https://static.vecteezy.com/system/resources/previews/000/378/201/original/sad-emoji-vector-icon.jpg" alt="" /> <br />
+      
       <Link to="/" className="text-center">Go back to home page</Link>
     </div>
   );
